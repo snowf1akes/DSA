e6b7004eda60b5a7aa7ec179089e96ac182bfcd3
Sunny Le <sunsuncatto1104@gmail.com>
2026-03-11 18:08:35 -0700

    factorial basics

---
+++ Algorithms/Factorial.py
@@ -0,0 +1,10 @@
+#Recursion: 5! -> 4! -> 3! -> 2! -> 1! = base case (end of loop)
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n-1);    
+}
+
+#time complexity  = O(n) 
